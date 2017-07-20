0a74451f88287f24edf929c2dc81eec1124149db
Kostadin <cvejoski@gmail.com>
2017-07-20 11:35:22 +0200

    OpenRoberta/robertalab#568 add automatic version in setup.wxs

--- Resources/USBInstaller/Windows/build.bat
+++ Resources/USBInstaller/Windows/build.bat
@@ -1,4 +1,3 @@
-set VERSION=%1
 "%WIX%bin\candle.exe" setup.wxs
 "%WIX%bin\light.exe" -out OpenRobertaNAOSetupDE_%VERSION%.msi -ext WixUIExtension -cultures:de-DE setup.wixobj
 "%WIX%bin\light.exe" -out OpenRobertaNAOSetupEN_%VERSION%.msi -ext WixUIExtension -cultures:en-US setup.wixobj
