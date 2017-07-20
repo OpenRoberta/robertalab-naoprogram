7d0b8cc9f465418a78ff84eb11b70d5d11ddd5be
Kostadin <cvejoski@gmail.com>
2017-07-20 10:57:42 +0200

    fix build bat

--- Resources/USBInstaller/Windows/build.bat
+++ Resources/USBInstaller/Windows/build.bat
@@ -1,5 +1,5 @@
 set VERSION=%1
-"%WIX%bin\candle.exe" setup.wxs resources.wxs
-"%WIX%bin\light.exe" -out OpenRobertaNAOSetupDE_%VERSION%.msi -ext WixUIExtension -cultures:de-DE setup.wixobj resources.wixobj -b ./resources
-"%WIX%bin\light.exe" -out OpenRobertaNAOSetupEN_%VERSION%.msi -ext WixUIExtension -cultures:en-US setup.wixobj resources.wixobj -b ./resources
+"%WIX%bin\candle.exe" setup.wxs
+"%WIX%bin\light.exe" -out OpenRobertaNAOSetupDE_%VERSION%.msi -ext WixUIExtension -cultures:de-DE setup.wixobj
+"%WIX%bin\light.exe" -out OpenRobertaNAOSetupEN_%VERSION%.msi -ext WixUIExtension -cultures:en-US setup.wixobj
 @pause
