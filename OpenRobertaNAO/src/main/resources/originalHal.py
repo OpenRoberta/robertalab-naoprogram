a39025a5ee0b75b0ea01694bc865fc062fef7dfa
Artem Vinokurov <artem.vinokurov@inf.h-brs.de>
2017-08-18 16:14:55 +0200

    OpenRoberta/robertalab#573 implemented a function that takes the vocabulary, and returns a recognised word from this vocabulary. It waits till the word is recognised with 100 ms sleep

--- OpenRobertaNAO/src/main/resources/originalHal.py
+++ OpenRobertaNAO/src/main/resources/originalHal.py
@@ -14,6 +14,7 @@ class SpeechRecognitionModule(ALModule):
         self.memory = ALProxy("ALMemory")
         self.vocabulary = ["roberta"]
         self.lastWordRecognized = ""
+        self.isWordRecognized = False
         self.asr.pause(1)
         self.asr.setVocabulary(self.vocabulary, True)
         self.asr.setAudioExpression(False)
@@ -33,7 +34,7 @@ class SpeechRecognitionModule(ALModule):
 
     def onWordRecognized(self, key, value, message):
         self.lastWordRecognized = self.memory.getData("LastWordRecognized")[0]
-        print(self.lastWordRecognized)
+        self.isWordRecognized = True
 
     def pauseASR(self):
         self.asr.pause(1)
@@ -46,6 +47,15 @@ class SpeechRecognitionModule(ALModule):
 
     def toggleAudioExpression(self, state):
         self.asr.setAudioExpression(state)
+
+    def recognizeWordFromDictionary(self, vocabulary):
+        self.pauseASR()
+        self.setVocabulary(vocabulary)
+        self.resumeASR()
+        while(!self.isWordRecognized):
+            time.sleep(0.1)
+        self.isWordRecognized = False
+        return self.lastWordRecognized
 
 class Hal(object):
 
@@ -102,7 +112,7 @@ class Hal(object):
             self.motion.setStiffnesses(bodypart, 1.0)
         elif status == 2:
             self.motion.setStiffnesses(bodypart, 0)
-            
+
     def autonomous(self, mode):
         if mode == 1:
             self.awareness.startAwareness()
