e84046a3651772a52bed2aa1069150f27a4a23b3
Artem Vinokurov <artem.vinokurov@iais.fraunhofer.de>
2018-02-28 09:52:14 +0100

    OpenRoberta/robertalab#770 updated NAO hal with python2 version of random number generation for double numbers

--- OpenRobertaNAO/src/main/resources/roberta/blockly_methods.py
+++ OpenRobertaNAO/src/main/resources/roberta/blockly_methods.py
@@ -60,9 +60,7 @@ class BlocklyMethods:
 
     @staticmethod
     def randDouble():
-        b = os.urandom(4)
-        val = b[0] << 24 | b[1] << 16 | b[2] << 8 | b[3]
-        return float(val) / 0xffffffff
+        return 1.0*int(os.urandom(4).encode('hex'), 16) / 0xffffffff
 
     @staticmethod
     def textJoin(*args):
