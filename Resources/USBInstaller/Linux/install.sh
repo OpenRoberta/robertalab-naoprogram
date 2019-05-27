deb75ece9265942ab66367b085a2a175529457af
Kostadin Cvejoski <cvejoski@gmail.com>
2019-05-27 08:48:32 +0200

    bump the version to v1.0.6

--- Resources/USBInstaller/Linux/install.sh
+++ Resources/USBInstaller/Linux/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-version="1.0.5"
+version="1.0.6"
 dir="$( pwd )"
 
 echo "[Desktop Entry]" > "/usr/share/applications/ORUSBNao.desktop"
--- Resources/USBInstaller/Windows/build.bat
+++ Resources/USBInstaller/Windows/build.bat
@@ -1,4 +1,4 @@
 "%WIX%bin\candle.exe" setup.wxs
-"%WIX%bin\light.exe" -out OpenRobertaNAOSetupDE_1.0.5.msi -ext WixUIExtension -cultures:de-DE setup.wixobj
-"%WIX%bin\light.exe" -out OpenRobertaNAOSetupEN_1.0.5.msi -ext WixUIExtension -cultures:en-US setup.wixobj
+"%WIX%bin\light.exe" -out OpenRobertaNAOSetupDE_1.0.6.msi -ext WixUIExtension -cultures:de-DE setup.wixobj
+"%WIX%bin\light.exe" -out OpenRobertaNAOSetupEN_1.0.6.msi -ext WixUIExtension -cultures:en-US setup.wixobj
 @pause
